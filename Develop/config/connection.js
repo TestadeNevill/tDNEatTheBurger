58c45f113c7f07ef8d668213853c2fc71fcc500a
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2020-10-20 19:37:45 -0400

    Change connection js

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,21 +1,38 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "210$9Dow",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "o2ugi4pa2qkmz0x8",
+        password: "aerql139sukxwdvn",
+        database: "sdndi4ldmgt8qcp9"
+    });
+};
+
+connection.connect();
+modules.exports = connection;
+
+// var connection = mysql.createConnection({
+//     host: "us-cdbr-east-02.cleardb.com",
+//     // port: 3306,
+//     user: "b4984841d878ea",
+//     password: "9a9d0b26",
+//     database: "heroku_9ca241f01be0d7d"
+// });
+
+
 
 // Make connection.
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+// connection.connect(function (err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+// });
 
-// Export connection for our ORM to use.
-module.exports = connection;
+// // Export connection for our ORM to use.
+// module.exports = connection;
